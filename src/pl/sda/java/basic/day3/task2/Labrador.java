48e052e3f1b0397c426ed5bf3f8cb3a3ed1fc693
Michal Jaszczyk <vlad.ldz@gmail.com>
2019-02-03 23:23:41 +0100

    Przykłady w kodzie. Dzień 3 i 4

---
+++ src/pl/sda/java/basic/day3/task2/Labrador.java
@@ -0,0 +1,13 @@
+package pl.sda.java.basic.day3.task2;
+
+public class Labrador extends Pies {
+
+    public Labrador() {
+        super("Labrador");
+    }
+
+    @Override
+    public String szczekaj() {
+        return "hau Labradora";
+    }
+}
---
+++ src/pl/sda/java/basic/day3/task2/Sznaucer.java
@@ -0,0 +1,13 @@
+package pl.sda.java.basic.day3.task2;
+
+public class Sznaucer extends Pies {
+
+    public Sznaucer() {
+        super("Sznaucer");
+    }
+
+    @Override
+    public String szczekaj() {
+        return "szczek szczek";
+    }
+}
